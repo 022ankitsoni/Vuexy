579c34ae7e3ff847792be4ad0ba7b2185d99d5f0
022ankitsoni <ankitsoni0605@gmail.com>
2024-09-07 19:30:41 +0530

    added

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ app.use(express.json({ limit: '50mb' }));
 
 app.use(express.static(path.join(__dirname, 'client/vuexy/build')));
 
+
 app.use('/',router);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 app.get('/', async (req, res) => {
     res.status(200).json({
       message: 'Hello',
