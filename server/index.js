fb29fc7dcf45237198910fc23d41a27f94d706e9
022ankitsoni <ankitsoni0605@gmail.com>
2024-09-07 19:35:04 +0530

    added

--- server/index.js
+++ server/index.js
@@ -8,12 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-app.use(express.static(path.join(__dirname, 'client/vuexy/build')));
+app.use(express.static(path.join(__dirname, '../client/vuexy/build')));
 
 
 app.use('/',router);
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/vuexy/build', 'index.html'));
   });
 app.get('/', async (req, res) => {
     res.status(200).json({
