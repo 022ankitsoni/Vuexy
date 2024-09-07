c41129589e5fa6e74a37f95a7a0721145913e67b
022ankitsoni <ankitsoni0605@gmail.com>
2024-09-07 20:49:24 +0530

    added

--- server/index.js
+++ server/index.js
@@ -3,17 +3,24 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/route.js';
 import path from 'path';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-app.use(express.static(path.join(__dirname, '../client/vuexy/build')));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Define the path to your build folder
+// console.log(__dirname)
+const buildPath = path.join(__dirname, "build");
 
 
 app.use('/',router);
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/vuexy/build', 'index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(buildPath, "index.html"));
   });
 app.get('/', async (req, res) => {
     res.status(200).json({
