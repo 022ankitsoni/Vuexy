172113e3beb4001d010c2cc9704a6bdf7e738d42
022ankitsoni <ankitsoni0605@gmail.com>
2024-09-07 19:27:17 +0530

    added

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ import connectDB from './database/db.js';
 import express from 'express';
 import cors from 'cors';
 import router from './routes/route.js';
+import path from 'path';
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use(express.static(path.join(__dirname, 'client/vuexy/build')));
 
 app.use('/',router);
 app.get('/', async (req, res) => {
